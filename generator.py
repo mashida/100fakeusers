f0844068d788cbd7c683da6caf553e2601ebef68
Nikita Serditov <serditov.na@gmail.com>
2018-08-08 23:35:59 +0300

    Cleaned out some trash code and trash files

--- generator.py
+++ generator.py
@@ -19,44 +19,9 @@ data = json.loads(content)
 for user in data['results']:
 	users.append({'name':user['name']['first'], 'lastname':user['name']['last'], 'email':user['email']})
 
-#So I use generated data in order to check if code works
-
-'''
-users = [
-{'name': 'Bridget', 'lastname': 'Bradtke', 'email': 'Antwan_Jacobson@hotmail.com'},
-{'name': 'Ole', 'lastname': 'Klein', 'email': 'Deshaun9@gmail.com'},
-{'name': 'Guido', 'lastname': 'Kassulke', 'email': 'Mariah.Turcotte@hotmail.com'},
-{'name': 'Catharine', 'lastname': 'Wolf', 'email': 'Bailee.Skiles53@gmail.com'},
-{'name': 'Michel', 'lastname': 'Breitenberg', 'email': 'Leanne.Larson32@yahoo.com'},
-{'name': 'Rodrick', 'lastname': 'Nitzsche', 'email': 'Tommie_Schinner0@gmail.com'},
-{'name': 'Bobby', 'lastname': 'Durgan', 'email': 'Tianna_Botsford@yahoo.com'},
-{'name': 'Laisha', 'lastname': 'Heathcote', 'email': 'Joel.Abshire42@yahoo.com'},
-{'name': 'Daron', 'lastname': 'Hamill', 'email': 'Albin2@yahoo.com'},
-{'name': 'Keara', 'lastname': 'Tremblay', 'email': 'Delphia_Schowalter@yahoo.com'},
-{'name': 'Osbaldo', 'lastname': 'Kessler', 'email': 'Angus.Johns@yahoo.com'},
-{'name': 'Mae', 'lastname': 'Carter', 'email': 'Nicklaus_Raynor9@yahoo.com'},
-{'name': 'Macey', 'lastname': 'Dietrich', 'email': 'Zion.Wuckert@gmail.com'},
-{'name': 'Lorena', 'lastname': 'Bartoletti', 'email': 'Alverta_Bednar@gmail.com'},
-{'name': 'Theron', 'lastname': 'Johns', 'email': 'Chase_Lemke@yahoo.com'},
-{'name': 'Shaylee', 'lastname': 'Welch', 'email': 'Raven.Russel93@hotmail.com'},
-{'name': 'Alexis', 'lastname': 'Pollich', 'email': 'Raoul.Stokes@yahoo.com'},
-{'name': 'Maurine', 'lastname': 'Macejkovic', 'email': 'Rosamond_Gerhold@yahoo.com'},
-{'name': 'Santina', 'lastname': 'Mayert', 'email': 'Cesar.Balistreri10@hotmail.com'},
-{'name': 'Aiyana', 'lastname': 'Lind', 'email': 'Emilia_McLaughlin@gmail.com'},
-{'name': 'Filomena', 'lastname': 'Lowe', 'email': 'Precious_Turcotte34@hotmail.com'},
-{'name': 'Edwardo', 'lastname': 'White', 'email': 'Tatum.Kuvalis51@yahoo.com'},
-{'name': 'Ethel', 'lastname': 'Blick', 'email': 'Mertie.Rodriguez15@gmail.com'},
-{'name': 'Lexus', 'lastname': 'Ernser', 'email': 'Bennie.Daniel27@hotmail.com'}
-]
-'''
-
 @app.route("/")
 def home():
     return render_template('home.html', users = users)
-
-@app.route("/about")
-def about():
-    return render_template('about.html')
 
 if __name__ == '__main__':
 	app.run(debug = True)
